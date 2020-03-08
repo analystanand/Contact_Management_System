0c787fdead7c788bcd43db781464011c0408459c
Parmanand Sahu <parmanand.nitrr@gmail.com>
2020-03-07 20:32:08 -0600

    change model and add migration

--- contact_management/contact_app/models.py
+++ contact_management/contact_app/models.py
@@ -5,34 +5,31 @@ from localflavor.us.models import USStateField, USZipCodeField
 
 class Contact(models.Model):
     Contact_id = models.AutoField(primary_key=True)
-    Fname = models.CharField(max_length=100, )
-    Mname = models.CharField(max_length=100)
-    Lname = models.CharField(max_length=100)
+    Fname = models.CharField(max_length=50)
+    Mname = models.CharField(max_length=50)
+    Lname = models.CharField(max_length=50)
 
 
 class Address(models.Model):
-    ADDRESS_TYPE_CHOICE = [('H', 'Home'), ('W', 'Work'), ('O', 'Other')]
     Address_id = models.AutoField(primary_key=True)
     Contact_id = models.ForeignKey('Contact', on_delete=models.CASCADE)
-    Address_type = models.CharField(choices=ADDRESS_TYPE_CHOICE)
-    Street_Address = models.CharField(max_length=200)
-    City = models.CharField(max_length=200)
+    Address_type = models.CharField(max_length=50)
+    Street = models.CharField(max_length=200)
+    City = models.CharField(max_length=100)
     State = USStateField()
     Zip = USZipCodeField(blank=True)
 
 
 class Phone(models.Model):
-    PHONE_TYPE_CHOICE = [('H', 'Home'), ('W', 'Work'), ('O', 'Other')]
     Phone_id = models.AutoField(primary_key=True)
     Contact_id = models.ForeignKey('Contact', on_delete=models.CASCADE)
-    Phone_type = models.CharField(choices=PHONE_TYPE_CHOICE)
+    Phone_type = models.CharField(max_length=50)
     Area_code = models.IntegerField(validators=[MaxValueValidator(999)])
     Number = models.IntegerField()
 
 
 class Date(models.Model):
-    DATE_TYPE_CHOICE = [('H', 'Birthday'), ('W', 'Anniversary'), ('O', 'Other')]
     Date_id = models.AutoField(primary_key=True)
     Contact_id = models.ForeignKey('Contact', on_delete=models.CASCADE)
-    Date_type = models.CharField(choices=DATE_TYPE_CHOICE)
+    Date_type = models.CharField(max_length=50)
     Date = models.DateTimeField()
